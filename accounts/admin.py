4d2425c0b7ea31498918b19eefdd6ef2837b4f83
n00bmaster68 <63935745+n00bmaster68@users.noreply.github.com>
2021-07-06 17:54:27 +0700

    change fields in 'add new user' form and 'edit user' form

--- accounts/admin.py
+++ accounts/admin.py
@@ -12,4 +12,19 @@ class AccountAdmin(UserAdmin):
 	list_filter = ("groups", "is_active")
 	fieldsets = ()
 
+	fieldsets = (
+        ('Personal Information', {'fields': ('email', 'username', 'sex', 'phone_num')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_admin')}),
+		('Roles', {'fields':('groups',)}),
+    )
+	# formfield_overrides = {
+    #     Account.about: {'widget': Textarea(attrs={'rows': 10, 'cols': 40})},
+    # }
+	add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'phone_num', 'sex', 'password1', 'password2', 'is_active', 'is_staff', 'is_admin')}
+         ),
+    )
+
 admin.site.register(Account, AccountAdmin)
